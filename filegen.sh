a07abb823cbb82730252e8500d2a5278e4e86e91
goyergeek <goyergeek@gmail.com>
2025-04-08 19:38:32 -0600

    Updates to arrays_and_loops.sh for readability, Updates to logging of filegen to include timestamps

--- filegen.sh
+++ filegen.sh
@@ -10,8 +10,16 @@
 
 # Instantiate an indexed array called directories and populate it
 # directories.
-directories=("$HOME/files" "$HOME/files2" "$HOME/templogs")
+dirRoot="$HOME/scripts/bash"
+directories=("$dirRooot/files/01" "$dirRoot/files/02" "$dirRoot/files/03" "$dirRoot/files/temp")
 
+# Instantiate a function called add_timestamp() which will
+# prepend a date and time to all redirected output when called.
+add_timestamp() {
+    while IFS= read -r line; do
+        printf "%(%Y-%m-%d %H:%M:%S.%3N | )T %s\n" -1 "$line"
+    done
+}
 
 # A for loop to iterate through the directories array
 # and see if the folder exists then create it if not.
@@ -23,11 +31,23 @@ done
 
 # A for loop to create a file for every number in the range
 # 1 to 1000, and name the files with current month day and
-# year, as well as the file number.
+# year, as well as the file number.  Files in this group
+# are empty.  
 for i in {1..1000}; do
     filedate=$(date +"%m%d%Y")
-    touch "$HOME/files/${filedate}_file#_$i.txt"
+    touch "$dirRoot/files/01/${filedate}_file#_$i.txt"
 done
+
+# A for loop to create a file for every number in the range
+# 1 to 1000, and name the files with current month day and 
+# year, as well as the file number.  These files will be 
+# populated with 150kb of random alphanumeric text. 
+for i in {1..1000}; do
+    filedate=$(date + %m%d%Y)
+    tr -dc A-Za-z0-9\\n </dev/urandom | head -c 150000> "$dirRoot/files/02/${filedate}_file#_$i.txt"
+done
+
+
 
 # Instantiate a variable called counter and set it to zero.  This
 # will be used to count the number of loop iterations in the while
@@ -40,12 +60,12 @@ while [ $counter -lt 100 ]; do
 
     # Find only files in /files or /files2 and move them verbosely 
     # to the opposite folder.
-    find ~/files/ -type f -print0 | xargs -0 mv -vt ~/files2  1>>~/templogs/stdout.log 2>>~/templogs/stderr.log
-    find ~/files2/ -type f -print0 | xargs -0 mv -vt ~/files  1>>~/templogs/stdout.log 2>>~/templogs/stderr.log
+    find "$dirRoot/files/01/" -type f -print0 |  xargs -0 mv -vt "$dirRoot/files/temp" | add_timestamp 1>>"$dirRoot/output/init_stdout.log" 2>&1 | add_timestamp "$dirRoot/output/init_stderr.log"
+    find "$dirRoot/files/temp/" -type f -print0 | xargs -0 mv -vt "$dirRoot/files/01" | add_timestamp 1>>"$dirRoot/output/init_stdout.log" 2>&1 | add_timestamp "$dirRoot/output/init_stderr.log"
     
     # Copy Non-existent files verbosely to generate stderr output.  
     # There shouldn't be any stdout output.  
-    cp -v "$HOME/temp$counter/anyfile$counter.txt" "$HOME/files" 1>>~/templogs/stdout.log 2>>~/templogs/stderr.log
+    cp -v "$dirRoot/files/temp$counter/anyfile$counter.txt" "$dirRoot/files" | add_timestamp 1>>"$dirRoot/output/init_stdout.log" 2>&1 | add_timestamp "$dirRoot/output/init_stderr.log"
 
     # Increment the counter variable by 1, so the while condition 
     # eventually evaluates to false.
@@ -55,5 +75,4 @@ done
 # List contents of the files folder with human readable file sizes 
 # to ensure files are back where they belong, and redirect stdout
 # and stderr streams to files.
-ls -lh ~/files 1>>~/templogs/files_output.txt 2>>~/templogs/files_output_errors.log
-
+ls -lhR "$dirRoot/files/" | add_timestamp 1>>"$dirRoot/output/init_files_folders.log" 2>&1 | add_timestamp "$dirRoot/output/inti_files_errors.log"
--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# This script is meant to be run after pulling from github.
-# It will set up the script directory and file structures.
-find ~/scripts/ -name "*.sh" -type f -print0 | xargs -0 chmod ug+x
+# This script is meant to be run after pulling from github.  It will initialize the directory
+# and file structures for use.
+find ~/scripts/bash/ -name "*.sh" -type f -print0 | xargs -0 chmod ug+x     # Set Scripts to be Executable by current user and group

929a7213ebf46156093739aa3b12dbd8fac29690
goyergeek <goyergeek@gmail.com>
2025-05-02 12:56:03 -0600

    Completed functionality for advanced_menu.sh, including menus.sh which holds the associative arrays used to display menus and handle selections.

--- examples/advanced_menu.sh
+++ examples/advanced_menu.sh
@@ -1,47 +1,36 @@
 #!/bin/bash
 
-# This example shows how to construct an advanced menu using
-# the concepts built in previous examples.  Functions are 
-# used to generate menus rather than explicitly writing
-# the menu each time.
+# This example shows a more advanced method of displaying, navigating, and executing menu options.  It uses three functions 
+# and one outside file to accomplish the task.  The outside file contains information for both displaying menus, and also 
+# what to do when a selection is made.
+ 
 
-# Declare Associative Arrays that will contain menu items.
-declare -A main_menu
-declare -A disk_menu
-declare -A hw_menu
-declare -A cfg_menu
-declare -A sample_menu
-declare -A sample_menu2
-declare -A sample_menu3
-declare -A sample_menu4
+# Read contents of menus.sh into the script file.  This makes it as though this code was part of the current script file.
+# See comments in menus.sh for additional details on file structure.
+. menus.sh
 
-# Populate associative arrays 
-main_menu=([1]="Disk Menu" [2]="Hardware Menu" [3]="Config Menu" [4]="Sample Menu 1" [5]="Sample Menu 2" [6]="Exit" ["Name"]="Main Menu")
-disk_menu=([1]="Block Devices" [2]="Contents of /dev" [3]="LVM Wizard" [4]="Mounted Filesystems" [5]="Disk Usage" [6]="Main Menu" ["Name"]="Disk Menu")
-hw_menu=([1]="Network Hardware" [2]="Input Hardware" [3]="Memory Hardware" [4]="Storage Hardware" [5]="CPU Hardware" [6]="Main Menu" ["Name"]="Hardware Menu")
-cfg_menu=([1]="Local Bash Config" [2]="Global Bash Config" [3]="System Bash Config" [4]="Systemd Config" [5]="FS Table" [6]="Main Menu" ["Name"]="Config Menu")
-sample_menu=([1]="AAAAAAAAAAAAAAAAAAA" [2]="AAAAAAAAAAAAAAAAAAAA" [3]="AAAAAAAAAAAAAAAAAAAA" [4]="AAAAAAAAAAAAAAAAAAA" [5]="AAAAAAAAAAAAAAAAAAAA" [6]="AAAAAAAAAAAAAAAAAAAA" ["Name"]="Sample Menu 1")
-sample_menu2=([1]="Option 1" [2]="Option 2" [3]="Option 3" [4]="Option 4" [5]="Option 5" [6]="Main Menu" ["Name"]="Sample Menu 2")
-sample_menu3=([1]="AAAAAAAAAAAAAAAAAAAA" [2]="AAAAAAAAAAAAAAAAAAAA" [3]="AAAAAAAAAAAAAAAAAAAA" [4]="AAAAAAAAAAAAAAAAAAAA" [5]="AAAAAAAAAAAAAAAAAAAA" [6]="AAAAAAAAAAAAAAAAAAAA" [7]="AAAAAAAAAAAAAAAAAAAA" [8]="AAAAAAAAAAAAAAAAAAAA" [9]="AAAAAAAAAAAAAAAAAAAA" [10]="AAAAAAAAAAAAAAAAAAAA" [11]="AAAAAAAAAAAAAAAAAAAA" [12]="AAAAAAAAAAAAAAAAAAAA" [13]="AAAAAAAAAAAAAAAAAAAA" [14]="AAAAAAAAAAAAAAAAAAAA" [15]="AAAAAAAAAAAAAAAAAAAA" [16]="AAAAAAAAAAAAAAAAAAAA" [17]="AAAAAAAAAAAAAAAAAAAA" [18]="AAAAAAAAAAAAAAAAAAAA" [19]="AAAAAAAAAAAAAAAAAAAA" [20]="AAAAAAAAAAAAAAAAAAAA" ["Name"]="Sample Menu 3")
-sample_menu4=([1]="AAAAAAAAAAAAAAAAAAAA" [2]="AAAAAAAAAAAAAAAAAAAA" [3]="AAAAAAAAAAAAAAAAAAAA" [4]="AAAAAAAAAAAAAAAAAAAA" [5]="AAAAAAAAAAAAAAAAAAAA" [6]="AAAAAAAAAAAAAAAAAAAA" [7]="AAAAAAAAAAAAAAAAAAAA" [8]="AAAAAAAAAAAAAAAAAAAA" [9]="AAAAAAAAAAAAAAAAAAAA" [10]="AAAAAAAAAAAAAAAAAAAA" [11]="AAAAAAAAAAAAAAAAAAAA" [12]="AAAAAAAAAAAAAAAAAAAA" [13]="AAAAAAAAAAAAAAAAAAAA" [14]="AAAAAAAAAAAAAAAAAAAA" [15]="AAAAAAAAAAAAAAAAAAAA" [16]="AAAAAAAAAAAAAAAAAAAA" [17]="AAAAAAAAAAAAAAAAAAAA" [18]="AAAAAAAAAAAAAAAAAAAA" [19]="AAAAAAAAAAAAAAAAAAAA" ["Name"]="Sample Menu 4")
+# display_menu() does exactly that. It takes menu data provided in menus.sh and uses it to display a nicely formatted 
+# menu to the screen along with a prompt.
 display_menu() {
+    clear
+    local ch=$(echo $'\u2591')
     local -n arr_ref=$1
     local col_count=1
-    local menu_space=("\t*%-89s*\n" "")
-    local menu_name=("\t*%-37s%-15s%-37s*\n" "" "${arr_ref[Name]}" "")
-    local menu_prompt=("\t*  %s :: " "Select an Option")
-    local opts_line=("\t*%-2s%3s  %-25s%3s  %-25s%3s  %-20s%-2s*\n\t*%89s*\n" "" )
+    local menu_space=("\t$ch%-89s$ch\n" "")
+    local menu_name=("\t$ch%-37s%-15s%-37s$ch\n" "" "${arr_ref[Name]}" "")
+    local menu_prompt=("\t$ch  %s :: " "Select an Option")
+    local opts_line=("\t$ch%-2s%3s  %-25s%3s  %-25s%3s  %-20s%-2s$ch\n\t$ch%89s$ch\n" "" )
     local sorted_keys=($(printf "%s\n" "${!arr_ref[@]}" | sort))
     unset 'sorted_keys[-1]'
     local sorted_keys=($(printf "%s\n" "${sorted_keys[@]}" | sort -n))
     local opts=${#sorted_keys[@]}
     local opts_count=1
-    printf "\n\n\n"
-    printf "\t%*s\n" 91 "" | tr ' ' '*'
+    printf "\n\n\n\t"
+    printf "$ch%.0s" {1..91};printf "\n"
     printf "${menu_space[@]}" 
     printf "${menu_name[@]}"
     printf "${menu_space[@]}" 
-    printf "\t%*s\n" 91 "" | tr ' ' '*'
+    printf "\t";printf "$ch%.0s" {1..91};printf "\n"
     printf "${menu_space[@]}" 
     for idx in "${!sorted_keys[@]}"; do
         local idxVal=${sorted_keys[$idx]}
@@ -51,7 +40,7 @@ display_menu() {
             local col_count=1
             local opts_line+=("")
             printf "${opts_line[@]}"
-            local opts_line=("\t*%-2s%3s  %-25s%3s  %-25s%3s  %-20s%-2s*\n\t*%89s*\n" "" )
+            local opts_line=("\t$ch%-2s%3s  %-25s%3s  %-25s%3s  %-20s%-2s$ch\n\t$ch%89s$ch\n" "" )
         elif [ $opts_count = $((${#sorted_keys[@]} + 1)) ] && [ $col_count -lt 3 ]; then
             local col_remain=$((3 - col_count))
             for x in $(seq "1" "$col_remain"); do
@@ -59,11 +48,35 @@ display_menu() {
             done
             local opts_line+=("")
             printf "${opts_line[@]}"
-            #col_count=$((col_count + 1))
         else 
             local col_count=$((col_count + 1))
         fi
     done
-    printf "\t%*s\n\n" 91 "" | tr ' ' '*'
+    printf "\t";printf "$ch%.0s" {1..91};printf "\n\n"
     printf "${menu_prompt[@]}"
-}
+} # End Of Function display_menu()
+
+
+generate_case() {
+    local -n opts_ref=$1
+    local cases=("${!opts_ref[@]}")
+    echo "case \"\$selection\" in"
+    for case in "${cases[@]}"; do
+        echo -e "\t$case) echo \"${opts_ref[$case]}\" ;;"
+    done
+    echo -e "\t*) echo \"No match\";;"
+    echo "esac"
+} # End Of Function generate_case()
+
+
+run_menu() {
+    quitbit=1
+    current="main_"
+    while [ $quitbit -eq 1 ]; do
+        display_menu $current"menu"
+        read -r selection
+        opts_arr=$current"opts"
+        command=$(eval $(generate_case $opts_arr))
+        eval "$command"
+    done
+} # End of Function: handle_options()
---
+++ examples/menus.sh
@@ -0,0 +1,212 @@
+#!/bin/bash 
+
+# Declare Associative Arrays that contain the menu items.
+declare -A main_menu=(
+    [1]="Disk Menu" 
+    [2]="Hardware Menu" 
+    [3]="Config Menu" 
+    [4]="Sample Menu 1" 
+    [5]="Sample Menu 2"
+    [6]="Sample Menu 3"
+    [7]="Sample Menu 4"
+    [8]="Exit" 
+    ["Name"]="Main Menu"
+)
+
+declare -A main_opts=(
+    [1]="current=\"disk_\""
+    [2]="current=\"hw_\""
+    [3]="current=\"cfg_\""
+    [4]="current=\"sample_\""
+    [5]="current=\"sample2_\""
+    [6]="current=\"sample3_\""
+    [7]="current=\"sample4_\""
+    [8]="quitbit=\"0\""
+)
+
+declare -A disk_menu=(
+    [1]="Contents of /dev" 
+    [2]="Block Devices" 
+    [3]="LVM Wizard" 
+    [4]="Filesystems" 
+    [5]="Parted Wizard" 
+    [6]="Main Menu" 
+    ["Name"]="Disk Menu"
+)
+
+declare -A disk_opts=(
+    [1]='ls -lha --color=always /dev | less -r'
+    [2]='lsblk -lpf | less'
+    [3]='echo "Not A Chance. HA!"'
+    [4]='printf "%s\n%s\n\n%s\n%s\n\n" "******** File System Usage ********" "$(df -h)" "******** Inode Usage ********" "$(df -hi)" | less'
+    [5]='echo "Nope, Not here either bub."'
+    [6]="current=\"main_\""
+)
+
+declare -A hw_menu=(
+    [1]="All Hardware" 
+    [2]="Network Hardware"
+    [3]="Storage Hardware" 
+    [4]="Memory Hardware" 
+    [5]="CPU Hardware" 
+    [6]="Main Menu" 
+    ["Name"]="Hardware Menu"
+)
+
+declare -A hw_opts=(
+    [1]='sudo lshw -short | less'
+    [2]='sudo lshw -C Network | less'
+    [3]='sudo lshw -C Storage | less'
+    [4]='sudo lshw -C Memory | less'
+    [5]='sudo lshw -C CPU | less'
+    [6]="current=\"main_\""
+)
+
+declare -A cfg_menu=(
+    [1]="Local Bash Config" 
+    [2]="Global Bash Config" 
+    [3]="System Bash Config" 
+    [4]="Systemd Config" 
+    [5]="FS Table" 
+    [6]="Main Menu" 
+    ["Name"]="Config Menu"
+)
+
+declare -A cfg_opts=(
+    [1]='sudo lshw -short | less'
+    [2]='sudo lshw -C Network | less'
+    [3]='sudo lshw -C Storage | less'
+    [4]='sudo lshw -C Memory | less'
+    [5]='sudo lshw -C CPU | less'
+    [6]="current=\"main_\""
+)
+
+declare -A sample_menu=(
+    [1]="AAAAAAAAAAAAAAAAAAAA" 
+    [2]="AAAAAAAAAAAAAAAAAAAA" 
+    [3]="AAAAAAAAAAAAAAAAAAAA" 
+    [4]="AAAAAAAAAAAAAAAAAAAA" 
+    [5]="AAAAAAAAAAAAAAAAAAAA" 
+    [6]="AAAAAAAAAAAAAAAAAAAA" 
+    ["Name"]="Sample Menu 1"
+)
+
+declare -A sample_opts=(
+    [1]='sudo lshw -short | less'
+    [2]='sudo lshw -C Network | less'
+    [3]='sudo lshw -C Storage | less'
+    [4]='sudo lshw -C Memory | less'
+    [5]='sudo lshw -C CPU | less'
+    [6]="current=\"main_\""
+)
+
+declare -A sample2_menu=(
+    [1]="Option 1" 
+    [2]="Option 2" 
+    [3]="Option 3" 
+    [4]="Option 4" 
+    [5]="Option 5" 
+    [6]="Main Menu" 
+    ["Name"]="Sample Menu 2"
+)
+
+declare -A sample2_opts=(
+    [1]='sudo lshw -short | less'
+    [2]='sudo lshw -C Network | less'
+    [3]='sudo lshw -C Storage | less'
+    [4]='sudo lshw -C Memory | less'
+    [5]='sudo lshw -C CPU | less'
+    [6]="current=\"main_\""
+)
+
+declare -A sample3_menu=(
+    [1]="AAAAAAAAAAAAAAAAAAAA" 
+    [2]="AAAAAAAAAAAAAAAAAAAA" 
+    [3]="AAAAAAAAAAAAAAAAAAAA" 
+    [4]="AAAAAAAAAAAAAAAAAAAA" 
+    [5]="AAAAAAAAAAAAAAAAAAAA" 
+    [6]="AAAAAAAAAAAAAAAAAAAA" 
+    [7]="AAAAAAAAAAAAAAAAAAAA" 
+    [8]="AAAAAAAAAAAAAAAAAAAA" 
+    [9]="AAAAAAAAAAAAAAAAAAAA" 
+    [10]="AAAAAAAAAAAAAAAAAAAA" 
+    [11]="AAAAAAAAAAAAAAAAAAAA" 
+    [12]="AAAAAAAAAAAAAAAAAAAA" 
+    [13]="AAAAAAAAAAAAAAAAAAAA" 
+    [14]="AAAAAAAAAAAAAAAAAAAA" 
+    [15]="AAAAAAAAAAAAAAAAAAAA" 
+    [16]="AAAAAAAAAAAAAAAAAAAA" 
+    [17]="AAAAAAAAAAAAAAAAAAAA" 
+    [18]="AAAAAAAAAAAAAAAAAAAA" 
+    [19]="AAAAAAAAAAAAAAAAAAAA" 
+    [20]="Main Menu" 
+    ["Name"]="Sample Menu 3"
+)
+
+declare -A sample3_opts=(
+    [1]='sudo lshw -short | less'
+    [2]='sudo lshw -C Network | less'
+    [3]='sudo lshw -C Storage | less'
+    [4]='sudo lshw -C Memory | less'
+    [5]='sudo lshw -C CPU | less'
+    [6]="current=\"main_\""
+    [7]="current=\"main_\""
+    [8]="current=\"main_\""
+    [9]="current=\"main_\""
+    [10]="current=\"main_\""
+    [11]="current=\"main_\""
+    [12]="current=\"main_\""
+    [13]="current=\"main_\""
+    [14]="current=\"main_\""
+    [15]="current=\"main_\""
+    [16]="current=\"main_\""
+    [17]="current=\"main_\""
+    [18]="current=\"main_\""
+    [19]="current=\"main_\""
+    [20]="current=\"main_\""
+)
+
+declare -A sample4_menu=(
+    [1]="AAAAAAAAAAAAAAAAAAAA" 
+    [2]="AAAAAAAAAAAAAAAAAAAA" 
+    [3]="AAAAAAAAAAAAAAAAAAAA" 
+    [4]="AAAAAAAAAAAAAAAAAAAA" 
+    [5]="AAAAAAAAAAAAAAAAAAAA" 
+    [6]="AAAAAAAAAAAAAAAAAAAA" 
+    [7]="AAAAAAAAAAAAAAAAAAAA" 
+    [8]="AAAAAAAAAAAAAAAAAAAA" 
+    [9]="AAAAAAAAAAAAAAAAAAAA" 
+    [10]="AAAAAAAAAAAAAAAAAAAA" 
+    [11]="AAAAAAAAAAAAAAAAAAAA" 
+    [12]="AAAAAAAAAAAAAAAAAAAA" 
+    [13]="AAAAAAAAAAAAAAAAAAAA" 
+    [14]="AAAAAAAAAAAAAAAAAAAA" 
+    [15]="AAAAAAAAAAAAAAAAAAAA" 
+    [16]="AAAAAAAAAAAAAAAAAAAA" 
+    [17]="AAAAAAAAAAAAAAAAAAAA" 
+    [18]="AAAAAAAAAAAAAAAAAAAA" 
+    [19]="AAAAAAAAAAAAAAAAAAAA" 
+    ["Name"]="Sample Menu 4"
+)
+
+declare -A sample4_opts=(
+    [1]="current=\"main_\""
+    [2]="current=\"main_\""
+    [3]="current=\"main_\""
+    [4]="current=\"main_\""
+    [5]="current=\"main_\""
+    [6]="current=\"main_\""
+    [7]="current=\"main_\""
+    [8]="current=\"main_\""
+    [9]="current=\"main_\""
+    [10]="current=\"main_\""
+    [11]="current=\"main_\""
+    [12]="current=\"main_\""
+    [13]="current=\"main_\""
+    [14]="current=\"main_\""
+    [15]="current=\"main_\""
+    [16]="current=\"main_\""
+    [17]="current=\"main_\""
+    [18]="current=\"main_\""
+    [19]="current=\"main_\""
+)

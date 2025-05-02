04960882363389a5cc685becdaad1100ef304232
goyergeek <goyergeek@gmail.com>
2025-05-02 14:02:48 -0600

    Adjusted option 4 command for disk_opts.  Printing incorrect output.

--- examples/menus.sh
+++ examples/menus.sh
@@ -38,7 +38,7 @@ declare -A disk_opts=(
     [1]='ls -lha --color=always /dev | less -r'
     [2]='lsblk -lpf | less'
     [3]='echo "Not A Chance. HA!";sleep 10'
-    [4]='fs1=$(df -h);fs2=$(df -hi);echo -e "Filesystem Space Usage\n$fs1\n\nFilesystem Inode Usage\n$fs2\n"'
+    [4]='fs1=$(df -h);fs2=$(df -hi);echo -e "Filesystem Space Usage\n"$fs1"\n\nFilesystem Inode Usage\n"$fs2"\n"'
     [5]='echo "Nope, Not here either bub.";sleep 10'
     [6]="current=\"main_\""
 )
